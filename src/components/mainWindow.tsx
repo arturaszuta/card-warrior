6bb0fe0d4ca236392e8679a33b772f897a4affe7
Artie <arturaszuta@gmail.com>
2020-10-16 23:12:30 -0400

    Trying to make react-stack-grid work with fullpagejs

--- src/components/mainWindow.tsx
+++ src/components/mainWindow.tsx
@@ -4,6 +4,7 @@ import GameMenu from "./GameMenu";
 import ReactFullPage from "@fullpage/react-fullpage";
 import GameCard from "./GameCard";
 import BarGraph from "./BarGraph";
+import SampleStack from "./sampleReactStack";
 
 
 
@@ -31,6 +32,7 @@ const MainWindow = (props: any) => {
                 <div className="section">
                     <div className="slide cardArea">
                         <button onClick={() => fullpageApi.moveSlideRight()}>Go to game area</button>
+                        <button onClick={() => fullpageApi.moveSectionDown()}>Go to game area</button>
                             <div className="cardAreaWrapper">
                                 <div className="heroWrapper">
                                     <BarGraph />
@@ -47,6 +49,9 @@ const MainWindow = (props: any) => {
                         <button onClick={() => fullpageApi.moveSlideLeft()}>Go to staging area</button>
                         Game Area
                     </div>
+                </div>
+                <div className="section">
+                    <SampleStack />
                 </div>
 
                 </ReactFullPage.Wrapper>
---
+++ src/components/sampleReactStack.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import StackGrid from "react-stack-grid";
+
+const SampleStack = () => {
+    return (
+      <StackGrid
+        columnWidth={150}
+      >
+        <div key="key1">Item 1</div>
+        <div key="key2">Item 2</div>
+        <div key="key3">Item 3</div>
+      </StackGrid>
+    );
+  }
+
+
+  export default SampleStack
