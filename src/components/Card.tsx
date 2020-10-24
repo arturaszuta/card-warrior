394624f53c0b4d0ef21a7c61c8dd9f9dc1f27244
Artie <arturaszuta@gmail.com>
2020-10-24 11:17:27 -0400

    Add row mappings to cards.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Paper, Grid } from "@material-ui/core";
 
 const Card = (props:any) => {
-    console.log(props)
+    const { active } = props;
   
     return(
-        <Grid item className="gridCard" onClick={() => {console.log("stop it!")}}>
+        <Grid item className={`gridCard ${props.active == true ? "active" : ""}`} onClick={() => {props.setActiveIndex(props.index)}}>
             <Paper className="card" elevation={12}  >
                 {props.index}
+                <br></br>
+                {props.row}
             </Paper>
         </Grid>
     )
--- src/components/Dungeon.tsx
+++ src/components/Dungeon.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { Container, Grid } from "@material-ui/core";
 import Card from "./Card";
-import { createBuilderStatusReporter } from "typescript";
 
-interface IDungeon {
+interface IDungeonProps {
     size: string;
   }
 
 
-const Dungeon = (props:IDungeon) => {
+const Dungeon = (props:IDungeonProps) => {
     const { size } = props;
     let sizeCount = 0;
+    const [mainState, setMainState] = useState({activeIndex: -1});
 
     const BuildDungeon = (size: string) => {
         switch(size) {
@@ -19,26 +19,62 @@ const Dungeon = (props:IDungeon) => {
             break;
         }
     }
+    
+    const setActiveIndex = (index: number) => {
+        setMainState({...mainState, activeIndex: index});
+    }
 
-    const BuildState = (size: number) => {
-        const tempArray = Array.from(Array(size).keys());
-        const stateArray = tempArray.map((e, index) => {CardIndex: index + 1; Active: false})
-        return stateArray;
+    const getRow = (index: number) => {
+        let row = 0;
+        const dividedIndex = index / 5;
+        if(dividedIndex < 1) {
+            row = 1;
+        } else 
+        if(dividedIndex >= 1 && dividedIndex < 2) {
+            row = 2;
+        }
+        if(dividedIndex >= 2 && dividedIndex < 3) {
+            row = 3;
+        }
+        if(dividedIndex >= 3 && dividedIndex < 4) {
+            row = 4;
+        }
+        if(dividedIndex >= 4 && dividedIndex < 5) {
+            row = 5;
+        }
+        if(dividedIndex >= 5 && dividedIndex < 6) {
+            row = 6;
+        }
+        if(dividedIndex >= 6 && dividedIndex < 7) {
+            row = 7;
+        }
+        if(dividedIndex >= 7 && dividedIndex < 8) {
+            row = 8;
+        }
+        if(dividedIndex >= 8 && dividedIndex < 9) {
+            row = 9;
+        }
+        if(dividedIndex >= 9 && dividedIndex < 10) {
+            row = 10;
+        }
+        return row;
     }
 
     BuildDungeon(size);
-    const stateArray = BuildState(sizeCount);
 
-    const [mainState, setMainState] = useState(stateArray);
 
     const cardArray = Array.from(Array(sizeCount).keys());
-    const allCards = cardArray.map((e, index) => {return <Card key={index} index={index + 1} active={false}/>})
+    const allCards = cardArray.map((e, index) => {
+        const row = getRow(index);
+        return <Card key={index} index={index} active={mainState.activeIndex == index ? true : false} setActiveIndex={setActiveIndex} row={row}/>
+    })
 
     return(
         <Grid className="dungeon-wrapper" spacing={2} container>
             {allCards}
         </Grid>
     )
+
 }
 
 export default Dungeon;
