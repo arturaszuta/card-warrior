b62fddfb48d82f8775a27a6422960fb5356c9c22
Artie <arturaszuta@gmail.com>
2020-10-24 22:14:56 -0400

    Add custom styling to unreachable cards.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Paper, Grid } from "@material-ui/core";
+import { Paper, Grid, makeStyles } from "@material-ui/core";
 
 const Card = (props:any) => {
     const { active, row, column, setActiveIndex, setActiveRow, setActivecolumn, index, reachable } = props;
+
+    const useStyles = makeStyles(theme => ({
+        paperRoot: {
+          backgroundColor: reachable ? "default" :"lightgray"
+        }
+      }));
+
+      const classes = useStyles();
   
     return(
         <Grid item className={`gridCard ${active == true ? "active" : ""}`} onClick={() => {setActiveIndex(index)}}>
-            <Paper className={`card ${reachable ? "reachable" : "unreachable"}`} elevation={12}  >
+            <Paper className={`card ${reachable ? "reachable" : "unreachable"}`} elevation={12}  classes={{ root: classes.paperRoot }}>
                 {index}
                 <br></br>
                 {row}
