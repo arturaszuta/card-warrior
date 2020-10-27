85fe6d238a3bd306f1fdcbe631859506add37e33
Artie <arturaszuta@gmail.com>
2020-10-27 00:27:50 -0400

    keep adding styles to stats cards.

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Grid } from "@material-ui/core"
+import  "../CSS/cards.css"
 
 const StatsCard = () => {
     return(
         <Grid item className="statsCard">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="stats-card-start">a</div>
+            <div className="stats-card-icon">b</div>
+            <div className="stats-card-text">c</div>
         </Grid>
     )
 }
